501d4156d173648c6b61135de493662d325a96ca
elsamanora <elsaramadania@gmail.com>
2021-01-19 17:20:59 +0700

    Update form_data_user.php

--- application/views/member/form_data_user.php
+++ application/views/member/form_data_user.php
@@ -19,7 +19,7 @@
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="hero-cap">
-                                <h2>Form Data User</h2>
+                                <h4>Form Data User</h4>
                                 <nav aria-label="breadcrumb ">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"> <a href="<?= base_url() . 'index.php/member/Home' ?>">Home</a></li>
@@ -33,6 +33,22 @@
             </div>
         </div>
         <!-- Hero End -->
+        <!-- Form Start-->
+        <form name="" method="" action="">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                
+                </div>
+            </div>
+        </form>
+        <!-- Form End -->
+
         <!-- About Me Start -->
         <!-- <div class="about-me pb-top">
             <div class="container">
